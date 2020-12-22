8dca919f6eb93232e2edf3270a01cdedc5bac2c1
Rihab Haroun <ha.rihab@live.com>
2020-12-22 13:57:45 +0100

    worked on the swipe calculation

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print ("Fuck you")
+print ("Hello is it me you re looning for?")
+
+a=2
+b=6
+print ('initial value')
+print ('a= ',a 'and b= ',b )
+
+c=b
+b=a
+a=c
+
+print('SwipedValues')
+print ('a= ',a 'and b= ',b )
